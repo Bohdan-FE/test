d5406b2fb83e49a05cbab56b0ef0c886a2cbb8b5
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-08-05 23:51:28 +0200

    dwcwc

--- src/js/supportUkraineData.js
+++ src/js/supportUkraineData.js
@@ -3,45 +3,54 @@ export const FONDS = [
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: null,
+    png: './img/supportUkraine/supportUkraine-1.png'
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
     img: null,
+    png: './img/supportUkraine/supportUkraine-2.png'
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
     img: null,
+    png: './img/supportUkraine/supportUkraine-3.png'
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
     img: null,
+    png: './img/supportUkraine/supportUkraine-4.png'
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
     img: null,
+    png: './img/supportUkraine/supportUkraine-5.png'
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
     img: null,
+    png: './img/supportUkraine/supportUkraine-1.png'
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
     img: null,
+    png: './img/supportUkraine/supportUkraine-1.png'
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
     img: null,
+    png: './img/supportUkraine/supportUkraine-1.png'
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: null,
+    png: './img/supportUkraine/supportUkraine-1.png'
   },
 ];
